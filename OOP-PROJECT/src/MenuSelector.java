aaa0e0aace288fc1d5e07396dc630e715f466de2
gusreinaos <90770844+gusreinaos@users.noreply.github.com>
2021-09-16 15:23:24 +0200

    New Class
    Created new Menu class

--- OOP-PROJECT/src/MenuSelector.java
+++ OOP-PROJECT/src/MenuSelector.java
@@ -2,7 +2,13 @@
 public class MenuSelector {
     public static void main(String[]args ){
 
-        
+        //Function that creates Main Menu
+        public static String main_Menu (String input){
+
+
+
+            return main_Menu;
+        }
 
 
     }
