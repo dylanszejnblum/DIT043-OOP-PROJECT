8001d28c1899fb32a119678b021fae40c86a55ed
gusreinaos <90770844+gusreinaos@users.noreply.github.com>
2021-09-16 23:29:02 +0200

    Roles
    Roles assignment

--- OOP-PROJECT/src/MenuSelector.java
+++ OOP-PROJECT/src/MenuSelector.java
@@ -3,6 +3,12 @@ public class MenuSelector {
     public static void main(String[]args ){
 
         //Function that creates Main Menu
+
+        //Oscar y Dylan: Main Menu
+        //Dylan: Item Options Menu
+        //Oscar: Reviews options Menu
+        //Nia: Transactions History options Menu
+
         public static String main_Menu (String input){
 
 
