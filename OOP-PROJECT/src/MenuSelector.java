2b5c7fb15d1f8ac73fd2a85898033e537bcba1d6
dylandevelopt <dylanszejnblum@gmail.com>
2021-09-21 14:22:52 -0300

    added a generic text menu for the asignment
    In order to automate and have a baseline menu i've created one so it can be replicated and changed in accordance to the rest of the project requirements.

--- OOP-PROJECT/src/MenuSelector.java
+++ OOP-PROJECT/src/MenuSelector.java
@@ -1,21 +1,43 @@
+import java.util.Scanner;
 
 public class MenuSelector {
     public static void main(String[]args ){
+    	int choice = -1;
+    	// Base line menu for the rest of tasks.
+    	// Open scanner 
+    	Scanner scanner = new Scanner(System.in);
 
-        //Function that creates Main Menu
-
-        //Oscar y Dylan: Main Menu
-        //Dylan: Item Options Menu
-        //Oscar: Reviews options Menu
-        //Nia: Transactions History options Menu
-
-        public static String main_Menu (String input){
-
-
-
-            return main_Menu;
-        }
-
-
+    	printMainMenu();
+    	
+    	choice = scanner.nextInt();
+    	MainMenuOptions(choice);
+    	
+    	scanner.close();
+    }
+    
+    public static void printMainMenu() {
+    	System.out.println("Main Menu: Please choose among the options below.\n" 
+    + "0. Close system.\n" + "1. Open Item options. \n" + "2. Open Review options. \n" + "3. Open Transaction History options.\n" + 
+    		" \n" + "\n" + 
+    				"Type an option number:\n" 
+    				);
+    }
+    
+    public static void MainMenuOptions(int option) {
+    	switch(option) {
+    	case 1:
+    		System.out.println("You choose the first option");
+    		break;
+    	case 2: 
+    		System.out.println("You choose the second option");
+    		break;
+    	case 3: 
+    		System.out.println("You choose the third option");
+    		break;
+    	case 4: 
+    		System.out.println("You choose the fourth option");
+    		break;
+    	
+    	}
     }
 }
