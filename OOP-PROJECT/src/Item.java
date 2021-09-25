ac79c8b295631dabc25b4f2856d96397ef9c3275
dylandevelopt <dylanszejnblum@gmail.com>
2021-09-25 11:29:12 -0300

    Added Update function
    And also added some minor details

--- OOP-PROJECT/src/Item.java
+++ OOP-PROJECT/src/Item.java
@@ -3,9 +3,9 @@ public class Item {
 	// Name , Price , Id 
 	public  String Name; 
 	public  float Price; 
-	public  int final ID;
+	public  int  ID;
 	
-	public Item(String Name , float Price , int final ID) {
+	public Item(String Name , float Price , int  ID) {
 		if(Price < 0 || ID <0 || Name != " ") {
 			throw new IllegalArgumentException("Invalid data for item.");
 		}
@@ -18,5 +18,9 @@ public class Item {
 	
 	// A function to update the Item Name and price
 	
-	
+	public void UpdateItem(int Price , String Name) {
+		this.Price = Price;
+		this.Name = Name;
+		
+	}
 }
