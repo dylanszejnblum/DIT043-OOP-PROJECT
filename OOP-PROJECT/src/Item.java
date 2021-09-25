3b57fdbe6d2771e0ae1dccb06295a9c84558aae8
gusniage02 <nia.ggeorg@gmail.com>
2021-09-25 17:09:12 +0300

    we did some small changes with dylan

--- OOP-PROJECT/src/Item.java
+++ OOP-PROJECT/src/Item.java
@@ -3,15 +3,16 @@ public class Item {
 	// Name , Price , Id 
 	public  String Name; 
 	public  float Price; 
-	public  int ID;
+	public  int final ID;
 	
-	public Item(String Name , float Price , int ID) {
-		if(Price < 0 || ID <0) {
-			throw new IllegalArgumentException("Price or id should be greater at least 1 ");
+	public Item(String Name , float Price , int final ID) {
+		if(Price < 0 || ID <0 || Name != " ") {
+			throw new IllegalArgumentException("Invalid data for item.");
 		}
 		this.Name = Name;
 		this.Price = Price;
 		this.ID = ID;
+		System.out.println("Item" + ID + "was registered successfully.");
 	}
 	
 	
