0578168101e1a7932bf202a149c3d9f07af99095
dylandevelopt <dylanszejnblum@gmail.com>
2021-10-03 12:09:46 -0300

    Added Nia's changes
    Due to a merge conflict i added Nia's change manually

--- OOP-PROJECT/src/Items.java
+++ OOP-PROJECT/src/Items.java
@@ -30,31 +30,62 @@ public class Items {
 		return Total;
 	}
 
-	//CHANGE REMOVE ITEM METHOD
-	ArrayList<Item> removeItem (int ID) {
-		Item I = GetItemById(ID);
-		for (int i = 0; i< ItemList.size(); i++) {
-			
+// I did changes to 2.5; 2.6; 2.7 
+	//remove items - 2.5
+
+	public String removeItem (int ID){
+
+		for(Item item : ItemList) {
+			if(item.GetId() == ID){
+
+				ItemList.remove(item);
+				//do we have like a null element on that position after removing?
+			}
+				return "Item" + ID + "was successfully removed.";
+			}
+			  else { return "Item" + ID + "could not be removed.";}
 		}
 
 	}
-	
-	String PrintSpecific(int ID) {
-		Item I = GetItemById(ID);
-		System.out.println(I.GetId() + ":" + I.GetName() + "  " + I.GetPrice() + "SEK.");
-		return null;
-	}
+
+//print specific item? - 2.6
+
+
+public void PrintSpecificItem(int ID) {
+	Item I = GetItemById(ID);
+
+	if (I == null) {
+		System.out.println("Item" + ID + "was not registered yet.");
+	} else{
+	//rounding steps, to 2nd decimal; ASK DYLAN!!!!!
+	double priceOfItem = I.GetPrice();
+	double scale = Math.pow(10, 2);
+	double roundingThePrice = Math.round(priceOfItem * scale) / scale;
+	roundingThePrice = priceOfItem;
+
+	System.out.println(I.GetId() + ":" + I.GetName() + "  " + priceOfItem + "SEK.");
+
+}
+		//print all registered items - 2.7
 	//we cannot put prints on other classes that are not main so
 	//this may need to be changed into a return statement using a toString()
 
-	public String printAllItems(){
-		String result = "";
-		for (int i = 0; i < ItemList.size(); i++) {
-			result += ItemList.GetID() + ": " + ItemList.GetName() + ". " + ItemList.GetPrice() + " SEK" + "\n";
-		}
-		return result;
-	}
+	public void printAllItems() {
+		System.out.println("All registered items:");
 
+		boolean empty = ItemList.isEmpty();
+
+		if (empty = true) {
+			System.out.println("No items registered yet.");
+
+		} else {
+
+			for (Item item : ItemList) {
+				System.out.println(item.GetId() + ":" + item.GetName + ". " + item.GetPrice + "SEK.");
+			}
+
+		}
+	}
 	public Items() {
 		
 	}
