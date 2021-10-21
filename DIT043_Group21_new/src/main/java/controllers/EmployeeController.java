1edbf8bea0c19f2568cf4258e0ccfa05b5ca4bc9
dylansz <dylanszejnblum@gmail.com>
2021-10-21 03:20:47 -0300

    added more functions
    Only thing left are the facade and the sorting algorithms for epics 3 and 4 after that is debuging time

--- DIT043_Group21_new/src/main/java/controllers/EmployeeController.java
+++ DIT043_Group21_new/src/main/java/controllers/EmployeeController.java
@@ -1,2 +1,45 @@
-package controllers;public class EmployeeController {
+package controllers;
+
+public class EmployeeController {
+    public ArrayList<Employee> employees;
+    public Employee employee;
+    public MathHelpers mathHelper;
+
+    public EmployeeController(){
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+    }
+
+    public boolean equals(Employee anotherEmployee){
+        if(anotherEmployee == this.employee){
+            return true;
+
+        } else if(anotherEmployee == null){
+            return false;
+
+        } else if ( anotherEmployee instanceof Employee ){
+            return (this.employee.getID().equals(anotherEmployee.getID()));
+
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return this.employee.getName() + "'s gross salary is " + this.employee.getInitialGrossSalary() + " SEK per month";
+    }
+
+    public String createEmployee (String ID, String name, double grossSalary) {
+        Employee employee = new Employee(ID, name, grossSalary);
+        return "Employee " + this.employee.getID() + " registered successfully.";
+
+    }
+
+    public double calculateNetSalary() {
+        double finalNetSalary = 0.0;
+        finalNetSalary= employee.getNetSalary() - (this.employee.getInitialGrossSalary() * employee.getTAX_PERCENTAGE());
+        return MathHelpers.truncateDouble(finalNetSalary);
+        // Add the truncate function into the helpers
+    }
+
+
 }
